00e801f2eb67ab1b2c584b34177afba1ea9e31d4
찬휘 <cksgnlcjswoo@naver.com>
2023-03-22 14:15:27 +0900

    Feat: redux 하다맘

--- front-end/flory/src/redux/modules/diary/diary-action.ts
+++ front-end/flory/src/redux/modules/diary/diary-action.ts
@@ -20,3 +20,23 @@ export const createDiaryAction = createAsyncThunk(
     }
   }
 );
+
+// 해당 유저의 연,월에 해당하는 일기목록 가져오기
+export const GetDiaryByDateAction = createAsyncThunk(
+  "GET_DIARY_DATE",
+  async ({id, year, month} : {id: number, year : number,month : number},{rejectWithValue}) => {
+    try {
+      const accessToken = localData.getAccessToken();
+      const axios = axiosInitializer();
+
+      const { data } = await axios.get(`/api/diary`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return data;
+    } catch (e) {
+      return rejectWithValue(e);
+    }
+  }
+);
